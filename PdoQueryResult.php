a2d44b048aea41d79846efed22ac9813200af37e
Carsten Witt <carsten.witt@gmail.com>
2013-07-12 21:46:07 +0200

    Initial commit

---
+++ PdoQueryResult.php
@@ -0,0 +1,55 @@
+<?php
+namespace NewFreetag\Databases;
+
+use \NewFreetag\Databases\QueryResultInterface;
+use \NewFreetag\Databases\QueryResultAbstract;
+
+use \InvalidArgumentException;
+
+
+/**
+ * PDO query result wrapper for PDOStatement objects.
+ *
+ * @author Carsten Witt <carsten.witt@gmail.com>
+ */
+class PdoQueryResult extends QueryResultAbstract
+implements QueryResultInterface
+{
+
+
+/**
+ * @param \PDOStatement $pdo_rs
+ * @uses  QueryResultAbstract::__construct()
+ * @throws InvalidArgumentException
+ */
+    public function __construct($pdo_rs)
+    {
+        if ($pdo_rs instanceOf \PDOStatement) {
+            parent::__construct($pdo_rs);
+        }
+        else {
+            throw new InvalidArgumentException("PDOStatement expected.");
+        }
+    }
+
+
+
+//  =======  Implement interface QueryResultInterface  ===========
+
+
+
+/**
+ * Returns the number of affected rows.
+ *
+ * @return int
+ * @uses   $result
+ * @uses   PDOStatement::rowCount()
+ */
+    public function affectedRows() {
+        return $this->result->rowCount();
+    }
+
+
+
+
+}
