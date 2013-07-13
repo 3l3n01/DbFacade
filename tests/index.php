7a8fd0fae623d1f26abb1a58a4e08ca9eabc12f5
Carsten Witt <carsten.witt@gmail.com>
2013-07-13 13:42:14 +0200

    Moved init stuff to config.php

--- tests/index.php
+++ tests/index.php
@@ -1,50 +1,23 @@
 <?php
-namespace NewFreetag\Tests;
-
-require_once '../vendor/autoload.php';
-require_once 'src/functions.php';
-
-use \DbAdapters\DatabaseAdapterAbstract;
-
-
-
-/****************************************
- *
- * Setup mockup database connection
- *
- ****************************************/
-$db_driver = 'mysql';
-$db_host   = 'localhost';
-$db_name   = 'NewFreetag';
-$db_user   = 'root';
-#$db_pass   = 'buropa19';
-$db_pass   = 'kiranu49';
-
+namespace DbAdapters\Tests;
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>NewFreetag | Test suite</title>
+    <title>DbAdapters | Test suite</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
 <h1>DbAdapters Test</h1>
 <?php
+foreach($adapters as $adapter):
+    echo "<h3>", get_class($adapter), "</h3>\n";
 
 try {  // ================= Testing area ====================
 
-$adodb_mysql_conn = getDatabaseADOConnection("mysql", $db_host, $db_name, $db_user, $db_pass, 'utf8', 'pconnect');
-$adodb_adapter    = DatabaseAdapterAbstract::factory($adodb_mysql_conn);
-
-$pdo_mysql_conn   = getPdoConnection("mysql", $db_host, $db_name, $db_user, $db_pass, 'utf8', 'pconnect');
-$pdo_adapter      = DatabaseAdapterAbstract::factory($pdo_mysql_conn);
-
-$adapters = array($adodb_adapter, $pdo_adapter);
-
-echo "Moin";
-echo pre_r( $pdo_adapter->query("SELECT word FROM sample_words WHERE 1 LIMIT 0,25")->getRows());
+$sql = "SELECT word FROM sample_words WHERE 1 LIMIT 0,3";
+$words = $adapter->query("SELECT word FROM sample_words WHERE 1 LIMIT 0,3")->getRows();
 echo "<hr>";
 
 ?>
@@ -57,6 +30,8 @@ catch (\Exception $e) {
     $e->getMessage(),       "<br />",
     $e->getTraceAsString(), "</div>\n";
 }
+
+endforeach; // Adapters
 ?>
 </body>
 </html>
