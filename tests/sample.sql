837ea9b33821d7218a7ee35b21fb9e79aea781ff
Carsten Witt <carsten.witt@gmail.com>
2013-07-16 19:12:14 +0200

    Added sample SQL

---
+++ tests/sample.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `words` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `word` varchar(256) DEFAULT NULL,
+  `random` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
